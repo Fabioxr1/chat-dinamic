2e126525f0b6aedf8e825a16d1b84a52ff25a8c2
Fabio Perrone <musicfeedinf@gmail.com>
2018-10-29 22:56:21 +0100

    chat

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = 5000
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-if (process.env.OPENSHIFT_NODEJS_PORT) {
+if (process.env.NODE_ENV === 'production') {
     var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
     var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
     server = app.listen(server_port, server_ip_address, function () {
